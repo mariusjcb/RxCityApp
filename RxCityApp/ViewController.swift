faf42d7795d5f39a17455996e2f98fa3965ab3c3
Marius Ilie <marius_ilie20082001@yahoo.com>
2018-02-15 11:30:19 +0200

    Initial Commit

---
+++ RxCityApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxCityApp
+//
+//  Created by Marius Ilie on 15/02/2018.
+//  Copyright © 2018 Marius Ilie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
